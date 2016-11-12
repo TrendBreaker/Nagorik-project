4d33b68377fab6bab6befabfb83a4f8dfe5d910d
Asif Saifuddin Auvi <auvipy@gmail.com>
2016-11-12 17:27:11 +0600

    added admin inyerface for exisitng models

--- nagorik/apinag/admin.py
+++ nagorik/apinag/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Issues, SubCategory, Category
+
+admin.site.register(Issues)
+admin.site.register(SubCategory)
+admin.site.register(Category)
